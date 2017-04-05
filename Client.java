e0b57f39c91f1e30ef0e566b11abe10e5c99f9c9
jpham197 <jpham197@gmail.com>
2017-04-05 16:22:30 -0400

    Got the exit option working

--- Client.java
+++ Client.java
@@ -3,21 +3,23 @@ import java.util.Scanner;
 public class Client {
 
 	public static void main(String[] args) {
-		Scanner user = new Scanner(System.in);
+
 
 		System.out.println("Welcome to the classroom management system! Command line edition");
 		System.out.println("Press M for menu options.");
-		keyPress(user); //takes input from user to see what user wants to do
+		keyPress(); //takes input from user to see what user wants to do
 
 	}
 
-	public static void keyPress(Scanner user) {
+	public static void keyPress() {
+		Scanner user = new Scanner(System.in);
 		Menu m = new Menu();
 		String enteredKey = "";
 		enteredKey = user.nextLine();
 
 		if (enteredKey.equals("m")||enteredKey.equals("M")) {
 			m.showMenu();
+			keyPress();
 		} else if (enteredKey.equals("e")||enteredKey.equals("E")){
 			System.exit(0);
 		}
